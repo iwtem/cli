93c0bad6252f4f778a0b006884e4a64c2af9965f
bin.hui <bin.hui@convertlab.com>
2022-06-13 00:31:26 +0800

    refactor: 重新调整 发布 release 命令，同时更新 github action 的 release 发布配置

--- bin/iwtem-cli.js
+++ bin/iwtem-cli.js
@@ -12,55 +12,30 @@ require('../lib/utils/check-node-version')();
 
 // 3. 加载 .env 文件
 const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+require('dotenv').config({ path: path.resolve(__dirname, '../', '.env') });
 
 const { program } = require('commander');
 const pkg = require('../package.json');
 
 program
-  .command('init')
-  .description('initialise a project with a prompt')
-  .option('-y, --yes', 'use default values')
-  .option('-f, --force', 'overwrite target directory if it exist')
-  .action((name, options) => {
-    require('../lib/command/init')(name, options);
-  });
-
-program
-  .command('upgrade')
-  .description('new version for current projects')
-  .argument('[givenVersion]', 'specify the upgraded version number')
-  .option('--patch', 'upgrade patch version')
-  .option('--prepatch', 'upgrade prepatch version')
-  .option('--minor', 'upgrade minor version')
-  .option('--premajor', 'upgrade premajor version')
-  .option('--major', 'upgrade major version')
-  .option('--premajor', 'upgrade premajor version')
-  .option('--prerelease', 'upgrade prerelease version')
-  .option('--release', 'is it a release version')
-  .action((givenVersion, options) => {
-    require('../lib/command/upgrade')(givenVersion, options).then((code) => {
-      process.exit(code);
-    });
-  });
-
-program
   .command('create')
   .argument('<app-name>', 'application name')
   .description('create a new project')
+  .option('--template', 'specify a template')
   .option('-f, --force', 'overwrite target directory if it exist')
   .action((name, options) => {
     require('../lib/command/create')(name, options);
   });
 
 program
-  .command('config [value]')
-  .description('inspect and modify the config')
-  .option('-g, --get <app-path>', 'get value from option')
-  .option('-s, --set <app-path> <value>')
-  .option('-d, --delete <app-path>', 'delete option from config')
-  .action((value, options) => {
-    console.log(value, options);
+  .command('release')
+  .description('publish a release version for the current project')
+  .argument('[givenVersion]', 'specify the upgraded version number')
+  .argument('[prereleaseId]', 'specify the prerelease id, such as beta, alpha, rc, etc.')
+  .action((givenVersion, prereleaseId) => {
+    require('../lib/command/release')(givenVersion, prereleaseId).then((code) => {
+      process.exit(code);
+    });
   });
 
 program.on('--help', () => {
